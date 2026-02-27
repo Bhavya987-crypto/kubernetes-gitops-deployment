ef2ffb46297d3fc45665a29e414a03bc83fdb71c
Bhavya <bhavyasrilinganagunta@gmail.com>
2026-02-26 21:27:39 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,23 @@
 terraform {
   required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+# Example VPC (placeholder for EKS expansion)
+resource "aws_vpc" "gitops_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "gitops-vpc"
+  }
 }
